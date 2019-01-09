3af6c6de002f6dfdd80bdf7afbf400578ba68e5f
junlee91 <jlee757@uic.edu>
2019-01-08 20:38:09 -0600

    Custom App

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,10 @@
 import Head from "next/head";
-import withLayout from "../lib/withLayout";
 
-const About = () => (
+export default () => (
   <div>
     <Head>
-      <title>About | Nomad Store</title>
+      <title>About | Soda Movie</title>
     </Head>
     <h1>About page</h1>
   </div>
 );
-
-export default withLayout(About);
--- pages/movie.js
+++ pages/movie.js
@@ -1,15 +1,14 @@
 import Head from "next/head";
 import { withRouter } from "next/router";
-import withLayout from "../lib/withLayout";
 
-const Post = props => (
+const Movie = props => (
   <div>
     <Head>
-      <title>{props.router.query.title} | Nomad Store</title>
+      <title>{props.router.query.title} | Soda Movie</title>
     </Head>
     <h1>{props.router.query.title}</h1>
     <p>lalalalalala</p>
   </div>
 );
 
-export default withLayout(withRouter(Post));
+export default withRouter(Movie);
