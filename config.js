e592b176219da874922be6213a76ef0f2416605e
junlee91 <jlee757@uic.edu>
2019-01-12 11:36:56 -0600

    Index Component

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 export const API_URL =
-  "https://api-useast.graphcms.com/v1/cjqpz6fhh0j1v01exba0vceho/master";
+  "https://api-euwest.graphcms.com/v1/cjmrqz1hg3g4001b9913bj7lk/master";
+// "https://api-useast.graphcms.com/v1/cjqpz6fhh0j1v01exba0vceho/master";
