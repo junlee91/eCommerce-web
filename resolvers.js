795eb06fe2a0c5b99e479eaec138107aa98ef0cc
junlee91 <jlee757@uic.edu>
2019-01-12 16:59:42 -0600

    Get Product from cache using resolvers

---
+++ resolvers.js
@@ -0,0 +1,23 @@
+import { gql } from "apollo-boost";
+import { PRODUCT_FRAGMENT } from "./fragment";
+
+export const defaults = {
+  cart: []
+};
+
+export const resolvers = {
+  Mutation: {
+    toggleProduct: (_, variables, { cache, getCacheKey }) => {
+      const id = getCacheKey({ __typename: "Product", id: variables.id });
+      const fragment = gql`
+        ${PRODUCT_FRAGMENT}
+      `;
+      const product = cache.readFragment({
+        fragment,
+        id,
+      });
+
+      console.log(product);
+    }
+  }
+};
