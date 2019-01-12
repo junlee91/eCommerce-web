ce42ecebd2bc2c27bd4d0b0e62484bf35c7f3b59
junlee91 <jlee757@uic.edu>
2019-01-11 22:39:20 -0600

    Base Component setting

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,20 @@
+import { Layout } from "antd";
 import App, { Container } from "next/app";
 import React from "react";
-import withApollo from "../lib/withApollo";
 import { ApolloProvider } from "react-apollo";
+
+import withApollo from "../lib/withApollo";
+
+const { Footer } = Layout;
 
 class MyApp extends App {
   static async getInitialProps({ Component, router, ctx }) {
     let pageProps = {};
+
     if (Component.getInitialProps) {
       pageProps = await Component.getInitialProps(ctx);
     }
+
     return { pageProps };
   }
 
@@ -18,7 +24,10 @@ class MyApp extends App {
     return (
       <ApolloProvider client={apollo}>
         <Container>
-          <Component {...pageProps} />
+          <Layout>
+            <Component {...pageProps} />
+            <Footer>This is important</Footer>
+          </Layout>
         </Container>
       </ApolloProvider>
     );
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,56 @@
-import { DatePicker } from "antd";
+import Head from "next/head";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import { Layout } from "antd";
+const { Content } = Layout;
 
-export default () => (
-  <h1>
-    index <DatePicker onChange={() => {}} />
-  </h1>
+export default ({ data }) => (
+  <>
+    <Head>
+      <title>Home | Soda Store</title>
+    </Head>
+    <Header
+      centerColumn={<h4>Soda Store</h4>}
+      leftColumn={<Button href="/search" text="Search" btnIcon={"search"} />}
+      rightColumn={
+        <Button href="/cart" text="Cart" btnIcon={"shopping-cart"} />
+      }
+    />
+    <Content style={{ padding: "0 50px" }}>
+      <div
+        style={{
+          display: "grid",
+          gridGap: "10px",
+          gridTemplateColumns: "repeat(auto-fit, minmax(100px, 1fr))",
+          width: "100%"
+        }}
+      >
+        <Button
+          href={`/category?name=men`}
+          hrefAs={`/category/men`}
+          text={"Men"}
+        />
+        <Button
+          href={`/category?name=women`}
+          hrefAs={`/category/women`}
+          text={"Women"}
+        />
+        <Button
+          href={`/category?name=shoes`}
+          hrefAs={`/category/shoes`}
+          text={"Shoes"}
+        />
+        <Button
+          href={`/category?name=suits`}
+          hrefAs={`/category/suits`}
+          text={"Suits"}
+        />
+        <Button
+          href={`/category?name=dresses`}
+          hrefAs={`/category/dresses`}
+          text={"Dresses"}
+        />
+      </div>
+    </Content>
+  </>
 );
