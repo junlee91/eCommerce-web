aaff7600967f6988be5d0be1c6d764a0ce5e2c81
junlee91 <jlee757@uic.edu>
2019-01-12 22:07:27 -0600

    Service Worker offline support

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,1 +1,20 @@
-setInterval(() => console.log("Hi!"), 5000);
+// execute once service worker is installed
+self.addEventListener("install", event => {
+  const offlinePage = new Request("/");
+  event.waitUntil(
+    fetch(offlinePage).then(response => {
+      return caches.open("soda-store").then(cache => {  // open cache
+        return cache.put(offlinePage, response);        // and store that offline page
+      });
+    })
+  );
+});
+
+// intercept all requests
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(error => {
+      return caches.open("soda-store").then(cache => cache.match("/"));
+    })
+  );
+});
