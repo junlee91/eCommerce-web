ff8b16392f970b013ab5081f13e2527350f20dc8
junlee91 <jlee757@uic.edu>
2019-01-13 15:21:51 -0600

    deploy heroku

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+let PORT = process.env.PORT || 3000;
+
 app
   .prepare()
   .then(() => {
@@ -34,9 +36,9 @@ app
       return handle(req, res);
     });
 
-    server.listen(3000, err => {
+    server.listen(PORT, err => {
       if (err) throw err;
-      console.log("> Ready on http://localhost:3000");
+      console.log(`> Ready on http://localhost:${PORT}`);
     });
   })
   .catch(ex => {
